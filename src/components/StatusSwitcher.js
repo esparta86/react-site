0b9bc24f88436b82fb0850c9f79c191a436dd3a6
fabrv <frivera@nevicad.com>
2021-05-25 09:41:18 -0600

    working aux codes

--- src/components/StatusSwitcher.js
+++ src/components/StatusSwitcher.js
@@ -1,44 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
+import GloggerService from '../services/GloggerService';
 
-const StatusSwitcher = () => {
-  return (
-    <Card>
-      <Card.Body>
-        <Card.Title className="text-primary">
-          <h1>Status Switcher</h1>
-        </Card.Title>
-        <Form>
-          <Form.Group>
-            <Form.Label>
-              Status
-            </Form.Label>
-            <Form.Control as="select">
-              <option>Calls</option>
-              <option>Email</option>
-              <option>Break</option>
-            </Form.Control>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>
-              Comment
-            </Form.Label>
-            <Form.Control placeholder="Type in additional comments" as="textarea" rows={2}/>
-          </Form.Group>
-          <Container>
-            <Row>
-              <Col>
-                <Button variant="secondary" size="lg" block>Save</Button>
-              </Col>
-              <Col>
-                <Button variant="outline-danger" size="lg" block>Clock Out</Button>
-              </Col>
-            </Row>
-          </Container>
-        </Form>
-      </Card.Body>
-    </Card>
-  )
+const auxesService = new GloggerService('/admin/aux-codes')
+
+class StatusSwitcher extends React.Component {
+  state = {
+    auxes: []
+  }
+
+  componentDidMount() {
+    auxesService.get()
+    .then(res => {
+      const auxes = res;
+      this.setState({ auxes });
+    })
+  }
+
+  render() { 
+    return (
+      <Card>
+        <Card.Body>
+          <Card.Title className="text-primary">
+            <h1>Status Switcher</h1>
+          </Card.Title>
+          <Form>
+            <Form.Group>
+              <Form.Label>
+                Status
+              </Form.Label>
+              <Form.Control as="select">
+                { this.state.auxes.map(a => <option value={a.id}>{a.attributes.name}</option>)}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>
+                Comment
+              </Form.Label>
+              <Form.Control placeholder="Type in additional comments" as="textarea" rows={2}/>
+            </Form.Group>
+            <Container>
+              <Row>
+                <Col>
+                  <Button variant="secondary" size="lg" block>Save</Button>
+                </Col>
+                <Col>
+                  <Button variant="outline-danger" size="lg" block>Clock Out</Button>
+                </Col>
+              </Row>
+            </Container>
+          </Form>
+        </Card.Body>
+      </Card>
+    )
+  }
 }
 
 export default StatusSwitcher
--- src/services/GloggerService.js
+++ src/services/GloggerService.js
@@ -1,8 +1,14 @@
+import axios from "axios"
 import config from "../config/config"
 
 export default class GloggerService {
   constructor (route) {
     this.route = route
     this.url = config.baseUrl + route
+  }
+
+  async get (limit = 1000, pagination_token = null) {
+    const res = ((await axios.get(this.url)).data).data
+    return res.filter((a) => a.attributes.active === true)
   }
 }
